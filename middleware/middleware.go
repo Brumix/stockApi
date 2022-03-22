f599d4ba5e347f45aee1058a03d32ea3697adb21
Brumix <brunompereira42@gmail.com>
2022-03-22 19:36:32 +0000

    [TASK] Added new middleware

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -3,6 +3,7 @@ package middleware
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
+	log "github.com/sirupsen/logrus"
 	"net/http"
 	"stockApi/errors"
 	service "stockApi/services"
@@ -22,6 +23,30 @@ func IsAuth() gin.HandlerFunc {
 			fmt.Println(claims)
 			ctx.Next()
 			return
+		}
+		errors.ErrorMiddleware("unauthenticated ", err)
+		ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"msg": "unauthenticated"})
+		ctx.Abort()
+		return
+
+	}
+}
+func IsAdmin() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		cookie, err := ctx.Cookie("authorization")
+		if err != nil {
+			errors.ErrorMiddleware(" Cookie don't exist ", err)
+			ctx.Abort()
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"msg": "unauthenticated"})
+			return
+		}
+		claims, err := service.ValidateCookie(cookie)
+		if err == nil {
+			log.Info(claims)
+			if claims.Role == "ADMIN" {
+				ctx.Next()
+				return
+			}
 		}
 		errors.ErrorMiddleware("unauthenticated ", err)
 		ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"msg": "unauthenticated"})
